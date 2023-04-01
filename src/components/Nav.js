40e4c2e3e4caafbdf6980e6d6aae50fd82bbd946
wangrui <wrTencent2022>
2023-04-01 16:04:13 +0800

    dev: self

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src="./logo192.png" />
+            <h3> ReactFacts</h3>
+            <h4> React course - Project 1</h4>
+        </nav>
+    )
+}
