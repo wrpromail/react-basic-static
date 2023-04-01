8dd56f2f7eae8cecfcf94204259f7d917ca9bc8e
wangrui <wrpromail@gmail.com>
2023-04-01 16:27:04 +0800

    dev: basic react code

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,16 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <h1>Main component</h1>
+        <main>
+            <h1 className="main-title"> Fun facts about react</h1>
+            <ul className="main-facts">
+                <li>Was first release in 2013</li>
+                <li>React is a JavaScript library</li>
+                <li>has over 100k starts on github</li>
+                <li>maintained by facebook</li>
+                <li>power thousands enterprise app</li>
+            </ul>
+
+        </main>
     )
 }
